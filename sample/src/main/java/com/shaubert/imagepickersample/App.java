aa1fef8ab911dd2d32ff8f6841c41ea05513d32b
shaubert <shaubert.alexander@gmail.com>
2017-07-11 22:25:48 +0200

    upd sample app

--- sample/src/main/java/com/shaubert/imagepickersample/App.java
+++ sample/src/main/java/com/shaubert/imagepickersample/App.java
@@ -1,13 +1,13 @@
 package com.shaubert.imagepickersample;
 
 import android.app.Application;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import com.bumptech.glide.BitmapRequestBuilder;
-import com.bumptech.glide.BitmapTypeRequest;
-import com.bumptech.glide.DrawableRequestBuilder;
-import com.bumptech.glide.DrawableTypeRequest;
+import com.bumptech.glide.*;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.target.Target;
 import com.shaubert.ui.imagepicker.ImagePickerConfig;
 import com.shaubert.ui.imagepicker.glide.GlideImageLoader;
 
@@ -21,17 +21,23 @@ public class App extends Application {
 
     private void setup() {
         GlideImageLoader imageLoader = new GlideImageLoader(this) {
-            protected BitmapRequestBuilder<Uri, Bitmap> configure(BitmapTypeRequest<Uri> request) {
-                return request
+            @Override
+            protected void loadBitmapWithGlide(Uri uri, Target<Bitmap> target) {
+                Glide.with(App.this)
+                        .load(uri)
+                        .asBitmap()
                         .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                        .animate(android.R.anim.fade_in);
+                        .animate(android.R.anim.fade_in)
+                        .into(target);
             }
 
             @Override
-            protected DrawableRequestBuilder<Uri> configure(DrawableTypeRequest<Uri> request) {
-                return request
+            protected void loadDrawableWithGlide(Context context, Uri uri, Target<GlideDrawable> target) {
+                Glide.with(context)
+                        .load(uri)
                         .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                        .animate(android.R.anim.fade_in);
+                        .animate(android.R.anim.fade_in)
+                        .into(target);
             }
         };
         ImagePickerConfig.setup()
